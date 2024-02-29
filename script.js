6c5d4dfe671c3863f8b63f66dba8ea0ccfe4e287
Guillaume HARTEMANN-PIOLLET <ghartemann@com-company.fr>
2024-02-29 12:44:17 +0100

    save

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const button = document.getElementById('button');
-const ok = document.getElementById('ok');
+const button = document.querySelector('#button');
+const ok = document.querySelector('#ok');
 
 button.onclick = deleteCookies;
-ok.style.display = 'none';
 
 function deleteCookies() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         const promise = chrome.cookies.getAll({name: 'PHPSESSID'}, function(cookies) {
             for (let cookie of cookies) {
-                chrome.cookies.remove({"url": tabs[0].url;, "name": cookie.name});
+                chrome.cookies.remove({"url": tabs[0].url, "name": cookie.name});
             }
         });
 
         Promise.resolve(promise).then(() => {
             ok.style.display = 'block';
+            button.style.display = 'none';
         });
     });
 }
